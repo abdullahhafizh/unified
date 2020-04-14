bd19eadc40aa78c92190602695cea1e158e24c78
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2020-04-14 13:36:59 +0700

    Enhance Logo/Text Receipt Text #4

--- _tTools/_SalePrintTool.py
+++ _tTools/_SalePrintTool.py
@@ -60,7 +60,7 @@ class PDF(FPDF):
         # Logo
         if os.path.isfile(LOGO_PATH):
             # self.image(name=LOGO_PATH, x=None, y=None, w=100, h=60, type='GIF')
-            self.image(LOGO_PATH, 20, 3, 50)
+            self.image(LOGO_PATH, 30, 5, 40)
         self.set_font(USED_FONT, '', GLOBAL_FONT_SIZE)
         self.ln(SPACING*3)
         # self.ln(SPACING)
