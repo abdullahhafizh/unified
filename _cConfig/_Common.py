68677ad1ce293dfc8e57c09367258552ce50ce97
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2021-03-01 19:31:19 +0700

    Try Handling Cashbox Full #4

--- _cConfig/_Common.py
+++ _cConfig/_Common.py
@@ -1427,6 +1427,7 @@ def generate_collection_data():
                     __['init_slot4'] = update['stock']
         __['collect_time'] = _Helper.time_string()
         BILL_ERROR = ''
+        _ConfigParser.get_set_value('BILL', 'last^money^inserted', 'N/A'),
         COLLECTION_DATA = __
     except Exception as e:
         LOGGER.warning(str(e))
