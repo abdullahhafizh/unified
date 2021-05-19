5d3f62882b713dab1896d149cbe851fd4540dc24
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2021-05-19 16:48:53 +0700

    Add Sorted Notes In Cash Activity

--- _cConfig/_Common.py
+++ _cConfig/_Common.py
@@ -334,7 +334,8 @@ def get_cash_activity(keyword=None, trx_output=False):
             # summary[n] = all_notes.count(n)
             summary.update({n:all_notes.count(n)})
             # LOGGER.debug((n, summary[n]))
-        sorted_summary = {k : summary[k] for k in sorted(summary)}
+        sorted_summary = {k : summary[k] for k in sorted(summary) if int(k) < 100000}
+        sorted_summary.update({'100000': summary.get('100000', 0)})
         output['summary'] = sorted_summary
         LOGGER.info(('CASH_STATUS', str(output)))
     except Exception as e:
