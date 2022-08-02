3dc4b20db2d3edce0cc01777a0703a17df5c9bdf
wahyudi <yudi@mdd.co.id>
2022-08-02 19:45:08 +0700

    Enhanve VLC Player

--- _pPlayer/vlc_player.py
+++ _pPlayer/vlc_player.py
@@ -1,24 +1,35 @@
-import vlc
- 
-# importing time module
-import time
-import sys
- 
-# creating vlc media player object
-media_player = vlc.MediaPlayer()
- 
-# toggling full screen
-media_player.toggle_fullscreen()
- 
-# media object
-media = vlc.Media("../_vVideo/2. \Video \Sosialisasi \Program \JakLingko.mp4")
- 
-# setting media to the media player
-media_player.set_media(media)
- 
-# start playing video
-media_player.play()
- 
-# wait so the video can be played for 5 seconds
-# irrespective for length of video
-time.sleep(5)
+from vlc import Instance
+import os
+from pathlib import Path
+
+
+class VLC:
+    def __init__(self):
+        self.Player = Instance('--loop')
+
+    def addPlaylist(self, path):
+        self.mediaList = self.Player.media_list_new()
+        medias = os.listdir(path)
+        for s in medias:
+            self.mediaList.add_media(self.Player.media_new(os.path.join(path,s)))
+        self.listPlayer = self.Player.media_list_player_new()
+        self.listPlayer.set_media_list(self.mediaList)
+    def play(self):
+        self.listPlayer.play()
+    def next(self):
+        self.listPlayer.next()
+    def pause(self):
+        self.listPlayer.pause()
+    def previous(self):
+        self.listPlayer.previous()
+    def stop(self):
+        self.listPlayer.stop()
+
+
+if __name__ == '__main__':
+    player = VLC()
+    up_dir = Path(__file__).resolve().parents[1]
+    print(up_dir)
+    path = up_dir
+    player.addPlaylist(path)
+    player.play()
