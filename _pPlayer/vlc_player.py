a8c5b2096f802741aefce1ad011a9fde822b1774
wahyudi <yudi@mdd.co.id>
2022-08-02 20:03:56 +0700

    Add User Click

--- _pPlayer/vlc_player.py
+++ _pPlayer/vlc_player.py
@@ -2,6 +2,30 @@ from vlc import Instance
 import os
 from pathlib import Path
 import time
+from pymouse import PyMouseEvent
+
+def fibo():
+    a = 0
+    yield a
+    b = 1
+    yield b
+    while True:
+        a, b = b, a+b
+        yield b
+
+
+class Clickonacci(PyMouseEvent):
+    def __init__(self):
+        PyMouseEvent.__init__(self)
+        self.fibo = fibo()
+
+    def click(self, x, y, button, press):
+        '''Print Fibonacci numbers when the left click is pressed.'''
+        if button == 1:
+            if press:
+                print(self.fibo.next())
+        else:  # Exit if any other mouse button used
+            self.stop()
 
 
 class VLC:
@@ -15,6 +39,7 @@ class VLC:
             self.mediaList.add_media(self.Player.media_new(os.path.join(path,s)))
         print('Media Length', len(self.mediaList))
         self.listPlayer = self.Player.media_list_player_new()
+        self.listPlayer.set_fullscreen(True)
         self.listPlayer.set_media_list(self.mediaList)
     def play(self):
         self.listPlayer.play()
@@ -29,10 +54,12 @@ class VLC:
 
 
 if __name__ == '__main__':
+    C = Clickonacci()
+    C.run()
     player = VLC()
     up_dir = Path(__file__).resolve().parents[1]
     path = os.path.join(up_dir, "_vVideo")
     player.addPlaylist(path)
     print(path)
     player.play()
-    time.sleep(30)
+    time.sleep(120)
