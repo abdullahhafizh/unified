13edcee8b2b5abb5737bf6b0ecaf7129dff46745
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2022-10-16 09:19:56 +0700

    15.2.1-GLOBAL - Add Chatbot Win #2

--- _chatbot-win.py
+++ _chatbot-win.py
@@ -91,10 +91,10 @@ def initiate():
 def main():
     global SOCKET_IO
     try:
-        print('Chat Host', INTERRACTIVE_HOST+'\n')
+        # print('Chat Host', INTERRACTIVE_HOST+'\n')
         host = INTERRACTIVE_HOST.replace('ws', 'http')
         port = INTERRACTIVE_HOST.split(':')[2]
-        SOCKET_IO = SocketIO(host, int(port))
+        SOCKET_IO = SocketIO(host, int(port), BaseNamespace)
         SOCKET_IO.emit('create', {
             'room': TID,
             'name': 'CONSOLE ' + TID,
