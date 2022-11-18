ca773501860a42cd25ee555c4d8d83eb917a63d1
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2022-11-18 14:13:20 +0700

    Fix  BRI Card Log #8

--- _mModule/_CPrepaidBRI.py
+++ _mModule/_CPrepaidBRI.py
@@ -649,41 +649,58 @@ def get_log_bri_priv(SAMSLOT, msg):
         Data = ""
         # resultStr, value = prepaid.topup_balance()
         # sleep(1)
-        resultStr, uid, cardno = prepaid.topup_get_sn()
-        LOG.fw("025:cardno = ",cardno)
-        LOG.fw("025:uid = ",uid)
+        # resultStr, uid, cardno = prepaid.topup_get_sn()
+        # LOG.fw("025:cardno = ",cardno)
+        # LOG.fw("025:uid = ",uid)
+        resultStr = prepaid.topup_card_disconnect()
         if resultStr == "0000":
             prepaid.topup_card_disconnect()
             resultStr, CardData = prepaid.topup_get_tokenbri()
             if resultStr == "0000":
                 resultStr, rapdu = prepaid.topup_apdusend("255", "91AF")
                 if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "6700":
+                    # Select AID
                     resultStr, rapdu = prepaid.topup_apdusend(SAMSLOT, "00A4040C09A00000000000000011")
                     if resultStr == "9000" or resultStr == "9100" or resultStr == "0000":
+                        # Card Select AID 1
                         resultStr, rapdu = prepaid.topup_apdusend("255", "905A00000301000000")
                         if resultStr == "9000" or resultStr == "9100" or resultStr == "0000":
+                            # CARD – Get Card Number, Perso Date & Issuer Code
                             resultStr, rapdu = prepaid.topup_apdusend("255", "90BD0000070000000017000000")
+                            cardno = rapdu[6:22]
+                            LOG.fw("025:cardno = ",cardno)
                             if resultStr == "9000" or resultStr == "9100" or resultStr == "0000":
+                                # CARD – Get Card Status
                                 resultStr, rapdu = prepaid.topup_apdusend("255", "90BD0000070100000020000000")
                                 resultStr = rapdu[6:10]
                                 if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "6161":
+                                    # CARD – Select AID 3
                                     resultStr, rapdu = prepaid.topup_apdusend("255", "905A00000303000000")
                                     if resultStr == "9000" or resultStr == "9100" or resultStr == "0000":
+                                        # CARD – Request Key Card 00
                                         resultStr, rapdu = prepaid.topup_apdusend("255", "900A0000010000")
                                         if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
-                                            sapdu = "80B0000020" + cardno + uid + "FF0000030080000000" + rapdu
-                                            resultStr, rapdu = prepaid.topup_apdusend(SAMSLOT, sapdu)
+                                            # CARD – Get UID
+                                            resultStr, rapdu = prepaid.topup_apdusend("255", "FFCA000000")
+                                            uid = rapdu
+                                            LOG.fw("025:uid = ",uid)
                                             if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
-                                                sapdu = rapdu[32:]
-                                                sapdu = "90AF000010" + sapdu + "00"
-                                                resultStr, rapdu = prepaid.topup_apdusend("255", sapdu)
+                                                # SAM – Authenticate Key
+                                                sapdu = "80B0000020" + cardno + uid + "FF0000030080000000" + rapdu
+                                                resultStr, rapdu = prepaid.topup_apdusend(SAMSLOT, sapdu)
                                                 if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
-                                                    resultStr, rapdu = prepaid.topup_apdusend("255", "90BB0000070100000000000000")
-                                                    resreport = resreport + rapdu
+                                                    # CARD – Authenticate Card
+                                                    sapdu = rapdu[32:]
+                                                    sapdu = "90AF000010" + sapdu + "00"
+                                                    resultStr, rapdu = prepaid.topup_apdusend("255", sapdu)
                                                     if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
-                                                        while resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
-                                                            resultStr, rapdu = prepaid.topup_apdusend("255", "90AF000000")
-                                                            resreport = resreport + rapdu
+                                                        # CARD – GET LOG TRANSATION
+                                                        resultStr, rapdu = prepaid.topup_apdusend("255", "90BB0000070100000000000000")
+                                                        resreport = resreport + rapdu
+                                                        if resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
+                                                            while resultStr == "9000" or resultStr == "9100" or resultStr == "0000" or resultStr == "91AF":
+                                                                resultStr, rapdu = prepaid.topup_apdusend("255", "90AF000000")
+                                                                resreport = resreport + rapdu
                 
                 n = 64
                 result1 = [resreport[i:i+n] for i in range(0, len(resreport), n)]
