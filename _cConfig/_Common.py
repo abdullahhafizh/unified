20cba4e2c8a4355bae32423ad05708fd0f066005
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2022-11-18 16:48:50 +0700

    Force BCA Dev in Develop Mode #1

--- _cConfig/_Common.py
+++ _cConfig/_Common.py
@@ -715,7 +715,7 @@ SERVICE_VERSION = _ConfigParser.get_set_value('TEMPORARY', 'service^version', '-
 COLOR_TEXT = _ConfigParser.get_set_value('TEMPORARY', 'color^text', 'white')
 COLOR_BACK = _ConfigParser.get_set_value('TEMPORARY', 'color^back', 'black')
 
-UPDATE_BALANCE_URL_DEV = 'http://apidev.mdd.co.id:28194/v1/'
+UPDATE_BALANCE_URL_DEV = 'http://192.168.8.60:28194/v1/'
 UPDATE_BALANCE_URL = _ConfigParser.get_set_value('GENERAL', 'update^balance^url', 'http://apiv2.mdd.co.id:2020/v1/')
 
 QR_HOST = _ConfigParser.get_set_value('QR', 'qr^host', 'http://apiv2.mdd.co.id:10107/v1/')
