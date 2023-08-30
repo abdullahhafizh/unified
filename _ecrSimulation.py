fb4d474f2b2b991e978fe54503a3c5744b4bf973
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2023-08-30 18:39:53 +0700

    Add ECR Simulator

---
+++ _ecrSimulation.py
@@ -0,0 +1,47 @@
+__author__ = "wahyudi@multidaya.id"
+
+from time import time
+import json
+import sys
+from _dDevice import _BCAEDC
+from _cConfig import _Common
+
+
+def welcome():
+    print("""
+            ECR Simulator
+        App Ver: """ + _Common.VERSION + """
+    Powered By: PT. MultiDaya Dinamika
+                -2023-
+    """)
+    
+    
+def simulate(amount=100, trxid=''):
+    result = None
+    sale_data = None
+    edc_ecr = _BCAEDC.BCAEDC()
+    trxid = ''.join(['SIM', str(time())])
+    if edc_ecr.connect(_Common.EDC_PORT):
+        amount = amount.replace('.00', '')
+        result, sale_data = edc_ecr.do_payment(trxid, amount)
+        print('Result:', result)
+        print('Data:', json.dumps(sale_data))
+    else:
+        print('ECR Not Connect', _Common.EDC_PORT)
+
+
+def exit(code=0, message='Exit Simulator'):
+    print(message+'\n')
+    sys.exit(code)
+
+
+if __name__ == '__main__':
+    welcome()
+    while True:
+        amount = input('Input Amount To Test?\n')
+        if int(amount) > 0:
+            amount = str(amount)
+            break
+    simulate(amount)
+    exit(0)
+    
