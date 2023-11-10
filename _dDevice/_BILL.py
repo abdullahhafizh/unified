a3fa57260e76a1a056e104f08fe70dd5bf6e4b22
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2023-11-10 21:43:44 +0700

    Enhance Reject Notes Func in NV #9

--- _dDevice/_BILL.py
+++ _dDevice/_BILL.py
@@ -362,12 +362,13 @@ def start_receive_note(trxid):
                             BILL_SIGNDLER.SIGNAL_BILL_RECEIVE.emit('RECEIVE_BILL|BAD_NOTES')
                             break
                         exceed_payment = is_exceed_payment(TARGET_CASH_AMOUNT, cash_in, COLLECTED_CASH)
-                        LOGGER.info(('Exceed Payment :', exceed_payment))
-                        if exceed_payment:
+                        LOGGER.info(('Exceed Payment :', BILL_TYPE, exceed_payment))
+                        # NV Cannot Reject Notes Which Enabled Without Special Hold Command
+                        if exceed_payment is True and BILL_TYPE != 'NV':
                             sleep(.5)
                             send_command_to_bill(param=BILL["REJECT"] + '|', output=None)
                             BILL_SIGNDLER.SIGNAL_BILL_RECEIVE.emit('RECEIVE_BILL|BAD_NOTES')
-                            LOGGER.info(('Exceed Payment Detected :', json.dumps({'REJECT': cash_in,
+                            LOGGER.warning(('Exceed Payment Detected :', json.dumps({'REJECT': cash_in,
                                                                                 'COLLECTED': COLLECTED_CASH,
                                                                                 'TARGET': TARGET_CASH_AMOUNT})))
                             break
