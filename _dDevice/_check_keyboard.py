4b08d0830718386a8e19ab87b2e72a84c9b3dafd
F. Wahyudi <fitrah.wahyudi.imam@gmail.com>
2024-05-08 12:05:53 +0700

    Parking Payment Dev #6

--- _dDevice/_check_keyboard.py
+++ _dDevice/_check_keyboard.py
@@ -1,19 +1,24 @@
 import keyboard
-import time
+# import time
+
+chars = ''
 
 def on_key_event(event):
+    global chars
     # Check if the event is a key press event        
     # Print the name of the pressed key
-    print("Pressed key:", event.name)
+    # print("Pressed key:", event.name)
+    if event.name not in keyboard.all_modifiers():
+        chars += event.name
 
 
 if __name__ == '__main__':
     # Set up the keyboard event listener
-    # keyboard.on_press(on_key_event)
-    while True:
-        event = keyboard.read_event()
-        print("Pressed key:", event.name)
-        time.sleep(.5)
-
+    keyboard.on_press(on_key_event)
+    # while True:
+    #     event = keyboard.read_event()
+    #     print("Pressed key:", event.name)
+    #     time.sleep(.5)
     # Keep the script running to continue listening for events
-    keyboard.wait('esc')
+    keyboard.wait('enter')
+    print(chars)
